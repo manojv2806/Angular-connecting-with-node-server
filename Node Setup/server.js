2eaf57e71146daa00b524430c5a899c4ee49bd24
Manoj V <46310861+manojv2806@users.noreply.github.com>
2020-10-24 13:51:37 +0530

    Update server.js

--- Node Setup/server.js
+++ Node Setup/server.js
@@ -4,8 +4,11 @@ const { strict } = require('assert');
 var express=require('express');
 var app = express();
 
-var routes = require('./routes/route1');
-app.use('/test', routes);
+var routes1 = require('./routes/route1');
+var routes2 = require('./routes/route2');
+app.use('/test1', route1); // url with test 1 goes to route1
+app.use('/test2', route2);// url with test 2 goes to route2
+
 app.use(express.static("myApp")); // myApp will be the same folder name.
 
 
